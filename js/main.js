e7b0489ca51816c3a65b5f806e96b0022a290400
M. Bohdan <marynych.b.o@gmail.com>
2023-11-09 16:59:53 +0200

    Added card images

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabsContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function (element) {
+	element.addEventListener('click', open);
+});
+
+function open(event) {
+	const tabTarget = event.currentTarget;
+	const button = tabTarget.dataset.button;
+
+	tabItem.forEach(function (item) {
+		item.classList.remove('tabs__btn-item--active');
+	});
+
+	tabTarget.classList.add('tabs__btn-item--active');
+
+	tabsContent.forEach(function (item) {
+		item.classList.remove('tabs__content-item--active');
+	});
+
+	document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+}
+
 const swiper = new Swiper('.swiper', {
 	effect: 'fade',
 	pagination: {
